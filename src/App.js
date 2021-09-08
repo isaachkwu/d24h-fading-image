2c9ce199444b06a106141559205af5e24e196107
D24H-WS-DELL-07\Isaac <abc>
2021-09-08 11:39:43 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import FadingImage from './FadingImage'
+import ImageA from './img/a.jpg'
+import ImageB from './img/b.jpg'
+import ImageC from './img/c.jpg'
+import ImageD from './img/d.jpg'
 
 function App() {
+  const images = [
+    ImageA, ImageB, ImageC, ImageD
+  ]
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +17,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <FadingImage 
+          style={{
+            height: '500px',
+            width: '100%'
+          }}
+          images={images}/>
         <a
           className="App-link"
           href="https://reactjs.org"
